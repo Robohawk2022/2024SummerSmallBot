c3f12dc8261920c87e9fbe3ccd1005625690586f
KailoskeuZhao <a98165809@gmail.com>
2024-05-17 08:57:15 -0400

    Small Robot Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,14 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -13,12 +20,27 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
+
+  private final CANSparkMax leftMotor = new CANSparkMax(2, MotorType.kBrushless);
+  private final CANSparkMax rightMotor = new CANSparkMax(1, MotorType.kBrushless);
+  private final XboxController control = new XboxController(0);
+
+  private final RelativeEncoder leftEncoder = leftMotor.getEncoder();
+  private final RelativeEncoder rightEncoder = rightMotor.getEncoder();
+
+  private final static double speedCoefficent = 0.75;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    leftMotor.setInverted(false);
+    rightMotor.setInverted(true);
+
+    leftMotor.setIdleMode(IdleMode.kBrake);
+    rightMotor.setIdleMode(IdleMode.kBrake);
+  }
 
   @Override
   public void robotPeriodic() {}
@@ -33,7 +55,24 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    double leftPercentage = 0.0;
+    double rightPercentage = 0.0;
+
+    leftPercentage = -control.getLeftY();
+    rightPercentage = - control.getRightY();
+
+    leftMotor.setVoltage(leftPercentage * speedCoefficent * 12.0);
+    rightMotor.setVoltage(rightPercentage * speedCoefficent * 12.0);
+
+    
+    SmartDashboard.putNumber("leftPercentage", leftPercentage);
+    SmartDashboard.putNumber("rightPercentage", rightPercentage);
+
+    SmartDashboard.putNumber("leftEncoderReportedVelocity", leftEncoder.getVelocity());
+    SmartDashboard.putNumber("rightEncoderReportedVelocity", rightEncoder.getVelocity());
+    
+  }
 
   @Override
   public void disabledInit() {}
